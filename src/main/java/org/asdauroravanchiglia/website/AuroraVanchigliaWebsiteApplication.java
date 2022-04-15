81077f4bc97cc64797033284378ebb82967cd305
Thomas <you@example.com>
2022-04-15 17:28:54 +0200

    Initial commit

---
+++ src/main/java/org/asdauroravanchiglia/website/AuroraVanchigliaWebsiteApplication.java
@@ -0,0 +1,13 @@
+package org.asdauroravanchiglia.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuroraVanchigliaWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuroraVanchigliaWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/asdauroravanchiglia/website/AuroraVanchigliaWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.asdauroravanchiglia.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuroraVanchigliaWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
